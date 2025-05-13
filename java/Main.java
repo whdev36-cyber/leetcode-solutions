2084ed61118a076ba519cc5c7593a5b4e15cc89b
WCyber <whdev36@gmail.com>
2025-05-13 10:29:23 +0500

    feat: Initialize Main class with Stack example

---
+++ java/Main.java
@@ -0,0 +1,9 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("Minicraft");
+    }
+}
